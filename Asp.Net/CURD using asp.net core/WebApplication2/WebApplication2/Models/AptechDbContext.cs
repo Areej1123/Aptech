3e2fa62600cb6ddf42c698b258b37972f73ba65e
Teacher <int@student-metro.edu>
2022-02-17 10:58:50 +0500

    crus using asp.net core

---
+++ Asp.Net/CURD using asp.net core/WebApplication2/WebApplication2/Models/AptechDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class AptechDbContext:DbContext
+    {
+        public AptechDbContext(DbContextOptions<AptechDbContext> options):base(options)
+        { 
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
